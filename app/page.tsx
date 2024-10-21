43b8f9353822ea760b79d23dc0093de1161b2f90
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-10-20 18:22:41 -0700

    update bg color

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ const HomePage = () => {
       <div id="home" className="px-10 py-16 w-full flex flex-col items-center justify-center gap-y-24">
         <Banner />
       </div>
-      <div id="how-it-works" className="px-10 py-16 w-full flex flex-col items-center justify-center gap-y-24">
+      <div id="how-it-works" className="px-10 py-16 w-full bg-color9 flex flex-col items-center justify-center gap-y-24">
         <Steps />
       </div>
-      <div id="about-us" className="px-10 py-16 w-full flex flex-col items-center justify-center gap-y-24">
+      <div id="about-us" className="px-10 py-16 w-full bg-color8 flex flex-col items-center justify-center gap-y-24">
         <Exhibit />
       </div>
       <div className="px-10 py-16 w-full bg-color3 text-white flex flex-col items-center justify-center gap-y-24">
