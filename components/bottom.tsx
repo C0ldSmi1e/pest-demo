1460d65a55316c9d91f68a99d2f24fbbf4a83628
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-12-29 19:53:01 -0800

    update responsive design

--- components/bottom.tsx
+++ components/bottom.tsx
@@ -3,17 +3,17 @@ import Review from "@/components/review";
 
 const Bottom = () => {
   return (
-    <div className="w-full max-w-screen-xl flex flex-col items-center justify-center gap-y-10 text-center">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-5xl font-bold leading-tight">
+    <div className="w-full max-w-screen-xl flex flex-col items-center justify-center gap-y-6 md:gap-y-10 px-4 md:px-6 text-center">
+      <div className="flex flex-col items-center justify-center gap-3 md:gap-4">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight">
           Why wait to protect your property, your health, and your family?
         </h1>
-        <p className="text-3xl">
+        <p className="text-xl md:text-2xl lg:text-3xl">
           Clean, dry, and pest-free homes are just a phone call away
         </p>
       </div>
       <Promote />
-      <div className="w-[50%]">
+      <div className="w-full md:w-3/4 lg:w-1/2">
         <Review />
       </div>
     </div>
