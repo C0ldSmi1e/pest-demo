0eb5a5e81becd8f71fc8f154a50c945489db8cdc
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-12-29 19:45:25 -0800

    update responsive design

--- components/steps.tsx
+++ components/steps.tsx
@@ -29,27 +29,27 @@ const Item = ({
   description: string;
 }) => {
   return (
-    <div className="w-full md:w-1/3 flex flex-col items-center justify-center gap-4">
+    <div className="w-full md:w-1/3 flex flex-col items-center justify-center gap-4 px-4">
       <Image src={image} alt={title} width={100} height={100} />
-      <h2 className="text-2xl font-bold">{title}</h2>
-      <p className="text-lg">{description}</p>
+      <h2 className="text-xl md:text-2xl font-bold">{title}</h2>
+      <p className="text-base md:text-lg">{description}</p>
     </div>
   );
 };
 
 const Steps = () => {
   return (
-    <div className="w-full max-w-screen-xl flex flex-col items-center justify-center gap-y-24 text-2xl text-center py-10">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-5xl font-bold">
+    <div className="w-full max-w-screen-xl flex flex-col items-center justify-center gap-y-12 md:gap-y-24 text-xl md:text-2xl text-center py-6 md:py-10 px-4">
+      <div className="flex flex-col items-center justify-center gap-2 md:gap-4">
+        <h1 className="text-3xl md:text-5xl font-bold">
           Get started in 
           <span className="text-color4"> 3 simple steps</span>
         </h1>
-        <p className="text-3xl">
+        <p className="text-xl md:text-3xl">
           It&apos;s this easy to protect your home from unwanted pests
         </p>
       </div>
-      <div className="flex flex-col md:flex-row items-start justify-center gap-y-16 gap-x-8">
+      <div className="flex flex-col md:flex-row items-start justify-center gap-y-12 md:gap-y-16 gap-x-8">
         {items.map((item) => (
           <Item key={item.title} {...item} />
         ))}
