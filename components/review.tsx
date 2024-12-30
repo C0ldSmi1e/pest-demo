cca91ccfdd477fb8b4afec88e29693109f407c6a
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-12-29 19:40:45 -0800

    update responsive design

--- components/review.tsx
+++ components/review.tsx
@@ -2,21 +2,21 @@ import Image from "next/image";
 
 const Review = () => {
   return (
-    <div className="w-full flex gap-4 items-start">
+    <div className="w-full flex flex-col sm:flex-row gap-4 items-center sm:items-start">
       <Image
-        className="rounded-full"
+        className="rounded-full w-20 h-20 sm:w-[120px] sm:h-[120px]"
         src="/images/person1.jpeg"
         alt="person1"
         width={120}
         height={120}
       />
-      <div className="flex flex-col gap-2 text-left">
-        <p className="text-xl">
+      <div className="flex flex-col gap-2 text-center sm:text-left">
+        <p className="text-base sm:text-xl">
           &quot;They did exactly what they said they would do. Polite people at
-        all levels. Nico explained what they could do with our attic insulation
+          all levels. Nico explained what they could do with our attic insulation
           inquiry...&quot;
         </p>
-        <div className="text-lg flex items-center gap-4">
+        <div className="text-base sm:text-lg flex flex-col sm:flex-row items-center gap-2 sm:gap-4">
           <span className="font-bold">John Doe</span>
           <div className="flex items-center gap-1">
             <Image
