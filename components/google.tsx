872075f07549b0971057d038418e2a93edfb46df
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2024-12-29 19:45:24 -0800

    update responsive design

--- components/google.tsx
+++ components/google.tsx
@@ -2,16 +2,18 @@ import Image from "next/image";
 
 const Google = () => {
   return (
-    <p className="font-semibold flex items-center gap-2">
+    <p className="font-semibold flex items-center flex-wrap gap-1 sm:gap-2 text-sm sm:text-base">
       <Image
         src="/images/google_icon.svg"
         alt="star"
         width={20}
         height={20}
+        className="w-4 h-4 sm:w-5 sm:h-5"
       />
       <span>4.9</span>
-      <span className="text-color2">★★★★★</span> Based on{" "}
-      <span className="font-bold">1000+</span> Google reviews
+      <span className="text-color2">★★★★★</span>
+      <span className="whitespace-nowrap">Based on</span>{" "}
+      <span className="font-bold whitespace-nowrap">1000+ reviews</span>
     </p>
   );
 };
