5b15c387f4cd9ea4c853a637156deae0cb17891f
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-02-01 17:34:34 -0800

    update bottom

--- components/bottom.tsx
+++ components/bottom.tsx
@@ -6,9 +6,9 @@ const Bottom = () => {
     <div className="w-full max-w-screen-xl flex flex-col items-center justify-center gap-y-6 md:gap-y-10 px-4 md:px-6 text-center">
       <div className="flex flex-col items-center justify-center gap-3 md:gap-4">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight">
-          Why wait to protect your property, your health, and your family?
+          Why wait to protect your <span className="text-yellow-400">property, health, and family</span>?
         </h1>
-        <p className="text-xl md:text-2xl lg:text-3xl">
+        <p className="text-xl md:text-2xl lg:text-3xl font-semibold text-gray-500">
           Clean, dry, and pest-free homes are just a phone call away
         </p>
       </div>
