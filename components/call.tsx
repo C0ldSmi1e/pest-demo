4745635c3a6b40bab17f890b8dd1830db7e1347d
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-02-01 17:43:57 -0800

    update call

--- components/call.tsx
+++ components/call.tsx
@@ -5,7 +5,10 @@ const Call = ({
   hoverTextColor = "hover:text-white",
 }) => {
   return (
-    <button className={`${bgColor} ${hoverBgColor} ${textColor} ${hoverTextColor} px-4 py-2 rounded-md hover:scale-105 font-bold transition-all duration-300 flex items-center justify-center gap-2`}>
+    <a
+      href="tel:0422808175"
+      className={`${bgColor} ${hoverBgColor} ${textColor} ${hoverTextColor} px-4 py-2 rounded-md hover:scale-105 font-bold transition-all duration-300 flex items-center justify-center gap-2`}
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-5 w-5"
@@ -15,7 +18,7 @@ const Call = ({
         <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
       </svg>
       0422808175
-    </button>
+    </a>
   );
 };
 
