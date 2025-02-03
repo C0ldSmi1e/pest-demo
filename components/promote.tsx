ae280898118063382f2a25cb09c61697dd6e07d6
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-02-02 23:11:09 -0800

    update text

--- components/promote.tsx
+++ components/promote.tsx
@@ -6,9 +6,6 @@ const Promote = () => {
   return (
     <div className="text-base flex flex-col items-center justify-center gap-8">
       <Google />
-      <p className="text-xl text-navy-900 font-bold">
-        Get a Free Quote Today!
-      </p>
       <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
         <Call />
         <Inspection />
