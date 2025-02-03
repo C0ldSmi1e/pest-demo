794334df2b76318c88cc0d15a129fc5e51b40f16
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-02-03 15:29:38 -0800

    update form

--- components/inspection.tsx
+++ components/inspection.tsx
@@ -5,11 +5,10 @@ const Inspection = () => {
     <>
       <button
         data-tally-open="nWKlOQ"
-        data-tally-align-left="1"
+        data-tally-layout="modal"
         data-tally-hide-title="1"
         data-tally-emoji-text="👋"
         data-tally-emoji-animation="wave"
-        data-tally-auto-close="3000"
         className="bg-yellow-400 text-white px-4 py-2 rounded-md shadow-lg font-bold hover:bg-yellow-500 hover:scale-105 transition-all duration-300"
       >
         Get a Free Quote
