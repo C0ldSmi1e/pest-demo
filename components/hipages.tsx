9a306e2737731b3d2c5080c1e7bacb2904bbc1de
Daniel Yu <40680511+Daniel777y@users.noreply.github.com>
2025-02-02 23:11:05 -0800

    update stars

--- components/hipages.tsx
+++ components/hipages.tsx
@@ -7,9 +7,15 @@ const HiPage = () => {
       href="https://hipages.com.au/connect/longlastpestcontrol"
       target="_blank"
       rel="noopener noreferrer"
-      className="font-semibold flex items-center flex-wrap gap-1 sm:gap-2 text-sm sm:text-base hover:text-yellow-400 transition-colors"
+      className="font-semibold flex flex-col justify-center items-center md:flex-row md:items-start gap-1 sm:gap-2 text-sm sm:text-base hover:text-yellow-400 transition-colors"
     >
-      <Star className="w-4 h-4 sm:w-5 sm:h-5 text-yellow-400 fill-yellow-400" />
+      <div className="flex items-center gap-1">
+        <Star className="w-4 h-4 sm:w-5 sm:h-5 text-yellow-400 fill-yellow-400" />
+        <Star className="w-4 h-4 sm:w-5 sm:h-5 text-yellow-400 fill-yellow-400" />
+        <Star className="w-4 h-4 sm:w-5 sm:h-5 text-yellow-400 fill-yellow-400" />
+        <Star className="w-4 h-4 sm:w-5 sm:h-5 text-yellow-400 fill-yellow-400" />
+        <Star className="w-4 h-4 sm:w-5 sm:h-5 text-yellow-400 fill-yellow-400" />
+      </div>
       <span>100% happy customers based on HiPages reviews</span>
     </Link>
   );
